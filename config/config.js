ec24877e55ec944a7fd83af3d08344ab833fb6e7
TixSpik <luisrhxq@hotmail.com>
2020-01-07 20:13:09 -0600

    enviroment updated

--- config/config.js
+++ config/config.js
@@ -9,11 +9,11 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 //DATABASE
 let urlBD
 
-// if (process.env.NODE_ENV === 'dev') {
-//     urlBD = 'mongodb://localhost:27017/cafe'
-// }
-// else {
-// }
-urlBD = 'mongodb+srv://tiktaktoe:35Kqmt7Q4OXDW6cK@cluster0-mh3ac.mongodb.net/cafe'
+if (process.env.NODE_ENV === 'dev') {
+    urlBD = 'mongodb://localhost:27017/cafe'
+}
+else {
+    urlBD = 'mongodb+srv://tiktaktoe:35Kqmt7Q4OXDW6cK@cluster0-mh3ac.mongodb.net/cafe'
+}
 
 process.env.URLDB = urlBD
